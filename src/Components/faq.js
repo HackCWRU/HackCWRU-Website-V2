de08b684382c9589fb032e38be159cb0cb0d6874
Daniel Grigsby <dbg28@case.edu>
2018-01-11 13:44:14 -0500

    Added beta of track components, some FAQ info

--- src/Components/faq.js
+++ src/Components/faq.js
@@ -25,49 +25,49 @@ class Faq extends Component {
 								<Row>
 									<Faq_component
 										title='Who can attend?'
-										paragraph='suppppppp'
+										paragraph='Anyone currently enrolled in a high school, undergraduate, or graduate program can attend.'
 									/>
 								</Row>
 								<br></br>
 								<Row>
 									<Faq_component
 										title='What if I have no experience?'
-										paragraph='suppppppp'
+										paragraph="First and foremost, hackathons are a learning experience. It wouldn't be fun if you knew exactly what to do. If you need help getting started or just can't find the source of a 'new feature' we have a team of mentors at your disposal."
 									/>
 								</Row>
 								<br></br>
 								<Row>
 									<Faq_component
 										title='What if I have no team or idea?'
-										paragraph='suppppppp'
+										paragraph='There are plenty of other hackers with your same dilemma that you can join forces with. Thirty six hours is plenty of time for a couple hours of brainstorming.'
 									/>
 								</Row>
 								<br></br>
 								<Row>
 									<Faq_component
 										title='Will travel be reimbursed?'
-										paragraph='suppppppp'
+										paragraph='Email us @ HackCWRU@case.edu, we plan on being able to reimbursing non-Case students for up to $50 of travel. Just make sure to save your travel receipt!'
 									/>
 								</Row>
 								<br></br>
 								<Row>
 									<Faq_component
-										title='How much $$$$$ I need?'
-										paragraph='suppppppp'
+										title='How much does it cost?'
+										paragraph='Nothing. Food, drinks, power, building materials, and machine time are provided for free.'
 									/>
 								</Row>
 								<br></br>
 								<Row>
 									<Faq_component
-										title='What bring doe?'
-										paragraph='suppppppp'
+										title='What should I bring?'
+										paragraph="Besides your computer, bring anything that you'd like to hack with or that would make your sleepover more comfortable. We suggest a sleeping bag, a toothbrush, and a change of clothes."
 									/>
 								</Row>
 								<br></br>
 								<Row>
 									<Faq_component
-										title='What if I has no toys to play with?'
-										paragraph='suppppppp'
+										title="What if I don't have equipment to make my hack?"
+										paragraph="We have that covered! We'll have laptops, 3D printers and a lot of MLH-provided hardare (including fancy VR stuff like the Oculus Rift!) provided for you to rent out for free."
 									/>
 								</Row>
 
