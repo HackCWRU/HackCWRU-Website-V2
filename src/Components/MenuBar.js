1eee7af85700fca6f6a6fe0e2bfc8917166bf752
Sacheth Chandramouli <c.sacheth1@gmail.com>
2018-01-14 14:09:57 -0500

    mlh logo fix

--- src/Components/MenuBar.js
+++ src/Components/MenuBar.js
@@ -12,14 +12,20 @@ class MenuBar  extends Component {
   render() {
     const mlh = {
       display:'block',
+      maxWidth:'100px',
+      minWidth:'60px',
       position:'absolute',
       right:'50px',
       top:'0',
-      width:'300%',
+      width:'10%',
+      zIndex:'10000'
     };
     const mlhLogo = {
       width:'100%',
     }
+
+    const imgLink = "https://s3.amazonaws.com/logged-assets/trust-badge/2018/gray.svg";
+    const link = "https://mlh.io/seasons/na-2018/events?utm_source=na-2018&utm_medium=TrustBadge&utm_campaign=na-2018&utm_content=gray";
     return(
       <div>
         <Navbar className="menuBar">
@@ -44,9 +50,7 @@ class MenuBar  extends Component {
           </Nav>
           <Nav pullRight>
 				<NavItem eventKey={1} href="#">
-          <a style={mlh} target="_blank">
-            <img src="https://s3.amazonaws.com/logged-assets/trust-badge/2018/gray.svg" style={mlhLogo} />
-          </a>
+          <a Id="mlh-trust-badge" style={mlh} href={link} target="_blank"><img src={imgLink} alt="Major League Hacking 2017 Hackathon Season" style={mlhLogo} /></a>
 				</NavItem>
 			</Nav>
         </Navbar>
