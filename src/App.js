444d8af377389ed9f3e19ab829bde3114baa44a6
Jack Mousseau <jackmousseau@gmail.com>
2018-02-13 20:49:32 -0500

    Fix typo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Hackry from 'hackry';
 
 import Intro_Section from './Components/intro_section';
 import About from './Components/About';
-import Tracks from './Components/Tracks';
+import Tracks from './Components/tracks';
 import FAQs from './Components/FAQs';
 import CarouselComponent from './Components/carouselcomponent';
 import Sponsors from './Components/sponsors';
