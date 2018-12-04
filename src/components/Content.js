bc13e26d1dc577a58f50fce9a9bee34e9259d288
IncredibLink <link@langchao.org>
2018-12-04 16:09:55 -0500

    Code linting

--- src/components/Content.js
+++ src/components/Content.js
@@ -4,28 +4,25 @@ import cutie from '../assets/cutie.png';
 class Content extends Component {
   render() {
     return (
-        <div className="mainContent">
-          <div className="theText">
-            <h1>HackCWRU 6</h1>
-            <h3>Case Western Reserve University</h3>
-            <h4>February 15-17th 2019</h4>
-            <h4>Registration Opening Soon</h4>
-            <div className="buttonGroup">
-              <a target="_blank" href="https://localhackday.mlh.io/lhd-2018/events/29-hackcwru-case-western-reserve-university" className="Button lhd">
-                <button>
-                  <p className="buttonTextLHD"> Local Hack Day </p>
-                </button>
-              </a>
-            </div>
+      <div className="mainContent">
+        <div className="theText">
+          <h1>HackCWRU 6</h1>
+          <h3>Case Western Reserve University</h3>
+          <h4>February 15-17th 2019</h4>
+          <h4>Registration Opening Soon</h4>
+          <div className="buttonGroup">
+            <a target="_blank" href="https://localhackday.mlh.io/lhd-2018/events/29-hackcwru-case-western-reserve-university" className="Button lhd">
+              <button>
+                <p className="buttonTextLHD"> Local Hack Day </p>
+              </button>
+            </a>
           </div>
-
-          <div className="myLogo">
-            <img src={cutie} />
-          </div>
-
-        
-
         </div>
+
+        <div className="myLogo">
+          <img src={cutie} />
+        </div>
+      </div>
     );
   }
 }
