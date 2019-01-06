873384045b719e6c55c5459227f14b3051fb7733
Sacheth Chandramouli <c.sacheth1@gmail.com>
2019-01-06 00:50:18 -0500

    first mock up
    first mock up of the website is completed just now. Mostly placement issues need to be considered now.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
+
 import Logo from './components/Logo.js';
 import Content from './components/Content.js';
 import FAQBar from './components/FAQBar.js';
+import Information from './components/Information.js';
+import Schedule from './components/Schedule.js';
+
 import Sea_sec1 from './components/Sea_sec1.js';
 import Sea_sec2 from './components/Sea_sec2.js';
-import Information from './components/Information.js';
+import Sea_sec3 from './components/Sea_sec3.js';
+import Sea_sec4 from './components/Sea_sec4.js';
+
 import clownFish from './Assets/new_assets/section_2/clown_fish.svg';
 import mantaRay from './Assets/new_assets/section_2/manta_ray.svg';
+
+import bubbles from './Assets/new_assets/section_3/bubbles.svg';
+import backgroundFish from './Assets/new_assets/section_3/backgroundFish.svg';
+import greenFish from './Assets/new_assets/section_3/greenFish.svg';
+import purpleFish from './Assets/new_assets/section_3/purpleFish.svg';
+
+import waveFish from './Assets/new_assets/section_4/fish_in_wave.svg';
+import backFish2 from './Assets/new_assets/section_4/2backFish.svg';
+import backFish3 from './Assets/new_assets/section_4/3fishBack.svg';
+import fishies from './Assets/new_assets/section_4/fishies.svg';
+import octopi from './Assets/new_assets/section_4/octopi.svg';
+
+import bottomBed from './Assets/new_assets/section_5/final.svg';
+
 import './App.css';
 
 class App extends Component {
@@ -31,11 +51,65 @@ class App extends Component {
         </section>
 
         <section className="section3">
-          <Information title="FAQ" text="" />
-          <div className="FAQContent">
-            <FAQBar question="" answer=""/>
+          <div className="eachSection">
+            <h3 className="title">FAQ</h3>
+            <div className="bubbleContainer">
+              <FAQBar question="What is a hackCWRU?" answer="hackCWRU brings together the brightest and most creative students to promote unrestricted technological innovation."/>
+              <FAQBar question="What is a hackCWRU?" answer="hackCWRU brings together the brightest and most creative students to promote unrestricted technological innovation."/>
+              <FAQBar question="What is a hackCWRU?" answer="hackCWRU brings together the brightest and most creative students to promote unrestricted technological innovation."/>
+              <FAQBar question="What is a hackCWRU?" answer="hackCWRU brings together the brightest and most creative students to promote unrestricted technological innovation."/>
+              <FAQBar question="What is a hackCWRU?" answer="hackCWRU brings together the brightest and most creative students to promote unrestricted technological innovation."/>
+              <FAQBar question="What is a hackCWRU?" answer="hackCWRU brings together the brightest and most creative students to promote unrestricted technological innovation."/>
+            </div>
+          </div>
+          <div className="bubbles">
+            <img src={bubbles}></img>
+          </div>
+          <div className="backgroundFish">
+            <img src={backgroundFish}></img>
+          </div>
+          <div className="greenFish">
+            <img src={greenFish}></img>
+          </div>
+          <div className="purpleFish">
+            <img src={purpleFish}></img>
+          </div>
+          <Sea_sec3 />
+        </section>
+
+        <section className="section4">
+          <div className="waveFish">
+            <img src={waveFish}></img>
+          </div>
+          <div className="eachSection">
+            <Schedule />
+          </div>
+
+
+          <div className="backFish2">
+            <img src={backFish2}></img>
+          </div>
+          <div className="backFish3">
+            <img src={backFish3}></img>
+          </div>
+          <div className="fishies">
+            <img src={fishies}></img>
+          </div>
+          <div className="octopi">
+            <img src={octopi}></img>
+          </div>
+          <Sea_sec4 />
+        </section>
+
+        <section className="section5">
+          <div className="eachSection">
+            <h3 className="darkTitle">Sponsorship</h3>
+          </div>
+          <div className="bottomBed">
+            <img src={bottomBed}></img>
           </div>
         </section>
+
       </div>
     );
   }
--- src/components/FAQBar.js
+++ src/components/FAQBar.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class FAQBar extends Component {
   render() {
     return (
-    <div className="container">
+    <div className="FAQBubble">
         <h3>{this.props.question}</h3>
         <p>{this.props.answer}</p>
     </div>
