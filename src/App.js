be5efcd368c1ea1349454ebc93404860be0f78a2
Sacheth Chandramouli <c.sacheth1@gmail.com>
2019-01-10 00:22:39 -0500

    added in real FAQS
    note: got rid of the FAQ about when hackCWRU starts and ends because there is a schedule right below the FAQ that is very easy to read

--- src/App.js
+++ src/App.js
@@ -87,12 +87,43 @@ class App extends Component {
           <div className="eachSection">
             <h3 className="darkTitle">FAQ</h3>
             <div className="bubbleContainer">
-              <FAQBar question="What is a hackCWRU?" answer="hackCWRU brings together the brightest and most creative students to promote unrestricted technological innovation."/>
-              <FAQBar question="What is a hackCWRU?" answer="hackCWRU brings together the brightest and most creative students to promote unrestricted technological innovation."/>
-              <FAQBar question="What is a hackCWRU?" answer="hackCWRU brings together the brightest and most creative students to promote unrestricted technological innovation."/>
-              <FAQBar question="What is a hackCWRU?" answer="hackCWRU brings together the brightest and most creative students to promote unrestricted technological innovation."/>
-              <FAQBar question="What is a hackCWRU?" answer="hackCWRU brings together the brightest and most creative students to promote unrestricted technological innovation."/>
-              <FAQBar question="What is a hackCWRU?" answer="hackCWRU brings together the brightest and most creative students to promote unrestricted technological innovation."/>
+              <FAQBar 
+                question="Where will hackCWRU V take place?"
+                answer="Hackers will be working in Horsburgh Gymnasium and Think[box]. Attendees can check in at Veale Center, 2138 Adelbert Rd, Cleveland, OH 44106."
+              />
+              <FAQBar 
+                question="Who can attend?" 
+                answer="Anyone currently enrolled in a high school, undergraduate, or graduate program can attend."
+              />
+              <FAQBar 
+                question="What if I don't have a lot of experience?" 
+                answer="First and foremost, hackathons are a learning experience.  It wouldn't be fun if you knew exactly what to do.  If you need help getting started or just can't find the source of a new feature we have a team of mentors at your disposal."
+              />
+              <FAQBar 
+                question="What if I don't have an idea or team?" 
+                answer="There are plenty of other hackers with your same dilemma that you can join forces with. Thirty six hours is plenty of time for a couple hours of brainstorming."
+              />
+              <FAQBar 
+                question="Will travel be reimbursed?" 
+                answer="We guarantee up to $50 in travel reimbursement per person. Just make sure to save your travel receipt!"
+              />
+              <FAQBar 
+                question="How much does it cost?" 
+                answer="Nothing.  Food, drinks, power, building materials, and machine time are provided for free."
+              />
+              <FAQBar 
+                question="What should I bring?" 
+                answer="Besides your computer, bring anything that you'd like to hack with or that would make your  sleepover more comfortable. We suggest a sleeping bag, a toothbrush, and a change of clothes. **Also, please bring a valid state ID or driver's license.**"
+              />
+              <FAQBar 
+                question="What if I don't have equipment to make my hack?" 
+                answer="We have that covered! We'll have laptops, 3D printers and a lot of MLH-provided hardare (including fancy VR stuff like the Oculus Rift!) provided for you to rent out for free."
+              />
+              <FAQBar 
+                question="Is there free parking?" 
+                answer="Yes! We have Lot 44 reserved on campus which is about a 5 minute walk from the venue."
+              />
+              
             </div>
           </div>
           <div className="bubbles">
@@ -107,13 +138,14 @@ class App extends Component {
           <div className="purpleFish">
             <img src={purpleFish}></img>
           </div>
+          <div className="waveFish">
+            <img src={waveFish}></img>
+          </div>
           <Sea_sec3 />
         </section>
 
         <section className="section4">
-          <div className="waveFish">
-            <img src={waveFish}></img>
-          </div>
+          
           <div className="eachSection">
             <Schedule events={this.state.events}/>
           </div>
