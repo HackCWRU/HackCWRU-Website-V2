6377da7644f5bd0b7051ed119a1b8e83c816784d
Sacheth Chandramouli <c.sacheth1@gmail.com>
2019-01-10 00:03:10 -0500

    Register Button
    Register button linked with Hackry

--- src/components/Content.js
+++ src/components/Content.js
@@ -8,8 +8,11 @@ class Content extends Component {
           <h1>hackCWRU 6</h1>
           <h3>Case Western Reserve University</h3>
           <h4>February 15-17th 2019</h4>
-          <h4>Registration Opening Soon</h4>
-          
+          <a target="_blank" href="https://dashboard.hackry.io/register?hackathonId=987tC2O5oK" className="Button lhd">
+            <button>
+              <p className="buttonTextLHD"> Register </p>
+            </button>
+          </a>
         </div>
       </div>
     );
